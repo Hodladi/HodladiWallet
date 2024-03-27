17b55470ad52e40a046471cb56e700d2ee8c7b76
Fredrik Bergqvist <fredrik.bergqvist@pri.se>
2024-03-27 15:11:09 +0100

    Fix set port to listen to from appsettings

--- Program.cs
+++ Program.cs
@@ -5,43 +5,44 @@ using HodladiWallet.Services;
 namespace HodladiWallet;
 public class Program
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
 
-        var phoenixApiConfig = new PhoenixApiConfig();
-        builder.Configuration.GetSection("PhoenixApi").Bind(phoenixApiConfig);
+		var phoenixApiConfig = new PhoenixApiConfig();
+		builder.Configuration.GetSection("PhoenixApi").Bind(phoenixApiConfig);
 
-        builder.Services.AddSingleton(phoenixApiConfig);
-        builder.Services.AddScoped<ILightningInvoiceDecoder, LightningInvoiceDecoder>();
+		builder.Services.AddSingleton(phoenixApiConfig);
+		builder.Services.AddScoped<ILightningInvoiceDecoder, LightningInvoiceDecoder>();
 
-        builder.Services.AddRazorComponents()
-            .AddInteractiveServerComponents();
+		builder.Services.AddRazorComponents()
+			.AddInteractiveServerComponents();
 
-        builder.Services.AddHttpClient<IInvoiceService, InvoiceService>((serviceProvider, client) =>
-        {
-            var config = serviceProvider.GetRequiredService<PhoenixApiConfig>();
-            client.BaseAddress = new Uri(config.BaseUrl);
-        });
+		builder.Services.AddHttpClient<IInvoiceService, InvoiceService>((serviceProvider, client) =>
+		{
+			var config = serviceProvider.GetRequiredService<PhoenixApiConfig>();
+			client.BaseAddress = new Uri(config.BaseUrl);
+		});
 
-        // Configure Kestrel to listen on port 5799 for HTTP
-        builder.WebHost.ConfigureKestrel(serverOptions =>
-        {
-            serverOptions.ListenAnyIP(5799); // Listen for HTTP connections on port 5799
-        });
+		var port = builder.Configuration.GetValue<int>("ApplicationSettings:Port");
 
-        var app = builder.Build();
+		builder.WebHost.ConfigureKestrel(serverOptions =>
+		{
+			serverOptions.ListenAnyIP(port);
+		});
 
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Error");
-            app.UseHsts();
-        }
-	    
-        app.UseStaticFiles();
-        app.UseAntiforgery();
-        app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+		var app = builder.Build();
 
-        app.Run();
-    }
+		if (!app.Environment.IsDevelopment())
+		{
+			app.UseExceptionHandler("/Error");
+			app.UseHsts();
+		}
+        
+		app.UseStaticFiles();
+		app.UseAntiforgery();
+		app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+		app.Run();
+	}
 }
