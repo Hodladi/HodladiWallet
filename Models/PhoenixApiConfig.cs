f092bc616e11cf43a9768a3fbabfcdf48225821e
Fredrik Bergqvist <fredrik@freber.dev>
2024-04-11 08:54:16 +0200

    Fix settings

--- Models/PhoenixApiConfig.cs
+++ Models/PhoenixApiConfig.cs
@@ -4,4 +4,6 @@ public class PhoenixApiConfig
 {
 	public string? BaseUrl { get; set; }
 	public string? Password { get; set; }
+	public bool LnUrlAddon { get; set; }
+	public string LnUrlUser { get; set; }
 }
